b73470f65e9d7ca8a6a8c4597d5713ebdc7415f6
JuneBin-Eom <ejb0329@gmail.com>
2021-07-18 17:17:33 +0900

    cloudstack API request

---
+++ cloudstackAuth/auth.py
@@ -0,0 +1,25 @@
+import urllib
+import hmac
+import hashlib
+import base64
+
+def make_request(requests, secretKey):
+  request = zip(requests.keys(), requests.values())
+  request.sort(key=lambda x: str.lower(x[0]))
+
+  requestUrl = "&".join(["=".join([r[0], urllib.quote_plus(str(r[1]))]) for r in request])
+  hashStr = "&".join(["=".join([str.lower(r[0]), str.lower(urllib.quote_plus(str(r[1]))).replace("+","%20")]) for r in request])
+  sig = urllib.quote_plus(base64.encodestring(hmac.new(secretKey, hashStr, hashlib.sha1).digest()).strip())
+  print("Signature:", sig)
+  requestUrl += "&signature="
+  requestUrl += sig
+  print(requestUrl)
+
+if __name__ == '__main__':
+  requests = {
+    "apiKey": "1y7awydp1lOnJSD6GAkryit-I9xT5KAak4fMVG5qBXAmyPLlv6dXJjB9xBA4kViooCwrproKsWldvTbRx2Puag",
+    "response" : "json",
+    "command" : "listHostsMetrics"
+  }
+  secretKey = "ZjzWOQCu1fkQOQado31rfBkk17LP0XpDTlULzMXOePMz4B94waPQytMVYkMLh6ZZC0ESdLIRDwqEs7xj-b69RQ"
+  make_request(requests, secretKey)
